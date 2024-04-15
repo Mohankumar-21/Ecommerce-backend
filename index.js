3b4a5cf947bd30659ef2e18cdc9eea335f5bc758
Mohankumar@LAPTOP-DSRRQ3HB <gmk9155@gmail.com>
2024-04-15 18:36:46 +0530

    first commit

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.post('/upload', upload.single('product'), (req,res)=>
    res.json(
     {
         success:1,
-        image_url:`http://localhost:${PORT}/images/${req.file.filename}`
+        image_url:`https://ecommerce-backend12.onrender.com/images/${req.file.filename}`
     }
    )
 })
